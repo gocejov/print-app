2dcfccbbd5f874fe7fc7cdbc9bf02e3fdbb146b5
Goce Jovancevski <goce.jovancevski@sentice.com>
2024-12-06 03:12:12 +0100

    render html video

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,6 +3,7 @@ import { IProductDocument } from '../models/product.model';
 import { ProductService } from '../services/product.service';
 import path from 'path';
 import { Request, Response } from 'express';
+import QRCode from 'qrcode';
 
 export interface IProductController extends ProductController { }
 
@@ -17,11 +18,42 @@ export class ProductController extends BaseController<IProductDocument> implemen
     const videoPath = path.resolve('uploads', 'videos', id);
 
     // Check if the video file exists (optional)
-    res.sendFile(videoPath, (err: any) => {
-      if (err) {
-        console.error('Error sending video:', err);
-        res.status(404).send('Video not found');
-      }
-    })
+    res.send(`
+      <video controls>
+        <source src="${videoPath}" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+      `)
+  }
+
+  // Route to generate the QR code
+  async getQrCode(req: Request, res: Response) {
+    const { id } = req.params; // Get the URL from query parameter (e.g., ?url=https://example.com)
+
+    if (!id) {
+      res.status(400).send('URL query parameter is required');
+      return
+    }
+
+    const url = `https://turl.world/api/products/videos/${id}`
+
+    // Generate the QR code as a data URL (image)
+    try {
+      const qrCodeDataURL = await QRCode.toDataURL(url);
+      res.send(`
+        <html>
+          <body>
+            <img src="${qrCodeDataURL}" alt="QR Code" />
+          </body>
+        </html>
+      `);
+    }
+    catch (err) {
+      console.error('Error generating QR code:', err);
+      res.status(500).send('Error generating QR code');
+    }
+
+    // Send the QR code image as a response
+
   }
 }
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -9,6 +9,7 @@ const router = Router();
 router.get('/', (req, res) => productController.getAll(req, res))
 router.get('/:id', (req, res) => productController.findById(req, res))
 router.get('/videos/:id', (req, res) => productController.getVideo(req, res))
+router.get('/qr-code/:id', (req, res) => productController.getQrCode(req, res))
 router.put('/:id', (req, res) => productController.update(req, res))
 router.post('/', (req, res) => productController.add(req, res))
 router.delete('/', (req, res) => productController.remove(req, res))
