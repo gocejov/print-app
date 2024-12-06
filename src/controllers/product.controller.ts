5753cd98529db88eeb8e3c5763f629132ec5dfa2
Goce Jovancevski <goce.jovancevski@sentice.com>
2024-12-06 02:33:21 +0100

    get video

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,17 +1,27 @@
 import { BaseController } from './base.controller';
 import { IProductDocument } from '../models/product.model';
 import { ProductService } from '../services/product.service';
+import path from 'path';
+import { Request, Response } from 'express';
 
-export interface IProductController extends ProductController {
-  login(): any
-}
+export interface IProductController extends ProductController { }
 
 export class ProductController extends BaseController<IProductDocument> implements IProductController {
   constructor() {
     super(new ProductService());
   }
 
-  login() {
-    throw new Error('Method not implemented.');
+  getVideo(req: Request, res: Response) {
+    const { id } = req.params
+    // Define the video file path
+    const videoPath = path.join(__dirname, 'uploads', 'videos', id);
+
+    // Check if the video file exists (optional)
+    res.sendFile(videoPath, (err: any) => {
+      if (err) {
+        console.error('Error sending video:', err);
+        res.status(404).send('Video not found');
+      }
+    })
   }
 }
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -8,6 +8,7 @@ const router = Router();
 
 router.get('/', (req, res) => productController.getAll(req, res))
 router.get('/:id', (req, res) => productController.findById(req, res))
+router.get('/videos/:id', (req, res) => productController.getVideo(req, res))
 router.put('/:id', (req, res) => productController.update(req, res))
 router.post('/', (req, res) => productController.add(req, res))
 router.delete('/', (req, res) => productController.remove(req, res))
