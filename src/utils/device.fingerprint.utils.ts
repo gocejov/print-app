87ab33d9330ea822e7b47c38acaccbffa50b8af3
Goce Jovancevski <goce.jovancevski@sentice.com>
2024-12-09 05:33:28 +0100

    updated version with some strategy for user identification nott completed

--- src/utils/device.fingerprint.utils.ts
+++ src/utils/device.fingerprint.utils.ts
@@ -1,8 +1,8 @@
-const  hash = require('crypto');
+import { hash } from 'crypto';
 import { Request } from 'express';
 
 export const DeviceFingerprint = {
-  get: (userAgent:any, req:Request) => {
+  get: (userAgent: any, req: Request) => {
     const screenWidth = req.headers['screen-width'] || 'unknown';
     const screenHeight = req.headers['screen-height'] || 'unknown';
     const language = req.headers['accept-language'] || 'unknown';
@@ -12,6 +12,7 @@ export const DeviceFingerprint = {
     const fingerprintString = `${userAgent}-${screenWidth}-${screenHeight}-${language}-${timezone}`;
 
     // Hash the fingerprint to get a unique identifier
-    return hash('sha256').update(fingerprintString).digest('hex');
+    // return hash('sha256').update(fingerprintString).digest('hex');
+    return hash("sha256", fingerprintString, "hex")
   }
 };
